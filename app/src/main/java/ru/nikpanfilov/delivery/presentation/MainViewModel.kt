53a31d57d0ea25c780dab009dd80a58d44e3e7f1
NikPanfilov <npanfilov2003@gmail.com>
2024-07-05 15:51:59 +0500

    feature/bottom-navigation Написал обработку интентов вьюмоделью

--- app/src/main/java/ru/nikpanfilov/delivery/presentation/MainViewModel.kt
+++ app/src/main/java/ru/nikpanfilov/delivery/presentation/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.SavedStateHandle
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import ru.nikpanfilov.delivery.core.navigation.Destination
 import ru.nikpanfilov.delivery.core.presentation.BaseViewModel
 import ru.nikpanfilov.delivery.core.token.domain.usecase.IsTokenExistUseCase
 
@@ -16,7 +17,32 @@ class MainViewModel @AssistedInject constructor(
 	override fun initState(): MainState = MainState(currentDestination = null)
 
 	override fun applyIntent(intent: MainIntent) {
+		when (intent) {
+			is MainIntent.OpenCalculation -> handleOpenCalculation()
+			is MainIntent.OpenHistory     -> handleOpenHistory()
+			is MainIntent.OpenProfile     -> handleOpenProfile()
+			is MainIntent.SetOpenedScreen -> handleSetOpenedScreen(intent.destination)
+		}
+	}
 
+	private fun handleOpenCalculation() {
+		router.navigateToCalculation()
+	}
+
+	private fun handleOpenHistory() {
+		router.navigateToHistory()
+	}
+
+	private fun handleOpenProfile() {
+		if (isTokenExistUseCase()) {
+			router.navigateToProfile()
+		} else {
+			router.navigateToSignIn()
+		}
+	}
+
+	private fun handleSetOpenedScreen(destination: Destination) {
+		setState(MainState(destination))
 	}
 
 	@AssistedFactory
