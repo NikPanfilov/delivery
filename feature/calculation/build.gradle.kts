625899db67fbd99b41febe4ee8c65dbfd33b4a7e
NikPanfilov <npanfilov2003@gmail.com>
2024-07-06 14:10:03 +0500

    feature/calculation Создал модуль расчёта

---
+++ feature/calculation/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+	alias(libs.plugins.library)
+	alias(libs.plugins.android.library)
+	alias(libs.plugins.compose.library)
+	alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+	namespace = "ru.nikpanfilov.delivery.feature.calculation"
+}
+
+dependencies {
+	implementation(project(":core:presentation"))
+	implementation(project(":core:ui"))
+	implementation(project(":core:token"))
+	implementation(project(":core:network"))
+	implementation(project(":core:navigation"))
+
+	implementation(libs.compose.ui)
+	implementation(libs.compose.material)
+	implementation(libs.compose.preview)
+	implementation(libs.compose.tooling)
+	implementation(libs.android.core)
+}
