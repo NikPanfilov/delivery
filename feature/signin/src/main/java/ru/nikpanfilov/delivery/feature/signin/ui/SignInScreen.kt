7036d369ce420f5f9ab6814e01c5708fb41a1e38
NikPanfilov <npanfilov2003@gmail.com>
2024-07-09 19:42:01 +0500

    feature/signin Написал ui входа

---
+++ feature/signin/src/main/java/ru/nikpanfilov/delivery/feature/signin/ui/SignInScreen.kt
@@ -0,0 +1,47 @@
+package ru.nikpanfilov.delivery.feature.signin.ui
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import ru.nikpanfilov.delivery.core.ui.compose.AppBar
+import ru.nikpanfilov.delivery.core.ui.compose.Error
+import ru.nikpanfilov.delivery.feature.signin.R
+import ru.nikpanfilov.delivery.feature.signin.presentation.LoadingStatus
+import ru.nikpanfilov.delivery.feature.signin.presentation.SignInIntent
+import ru.nikpanfilov.delivery.feature.signin.presentation.SignInState
+
+@Composable
+fun SignInScreen(
+	state: SignInState,
+	applyIntent: (SignInIntent) -> Unit,
+) {
+	Scaffold(
+		topBar = {
+			AppBar(
+				title = stringResource(R.string.sign_in),
+				leftIcon = null,
+				onLeftButtonClick = {
+					applyIntent(SignInIntent.NavigateBack)
+				},
+			)
+		}
+	) {
+		Box(
+			modifier = Modifier
+				.fillMaxSize()
+				.padding(it),
+		) {
+			when (state.loadingStatus) {
+				is LoadingStatus.Loading -> CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+				is LoadingStatus.Error   -> Error(onTryAgain = { applyIntent(SignInIntent.Next) })
+				is LoadingStatus.None    -> SignInContent(state = state, applyIntent = applyIntent)
+			}
+		}
+	}
+}
