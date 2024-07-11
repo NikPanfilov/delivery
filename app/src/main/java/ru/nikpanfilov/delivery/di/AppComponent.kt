fcb21b6a30b10e19c7ae275bb7ae74918adb83ed
NikPanfilov <npanfilov2003@gmail.com>
2024-07-11 22:44:38 +0500

    feature/shipping-method Добавил логику способа доставки в di

--- app/src/main/java/ru/nikpanfilov/delivery/di/AppComponent.kt
+++ app/src/main/java/ru/nikpanfilov/delivery/di/AppComponent.kt
@@ -17,6 +17,7 @@ import ru.nikpanfilov.delivery.feature.signin.di.SignInModule
 import ru.nikpanfilov.delivery.feature.signin.presentation.SignInViewModel
 import ru.nikpanfilov.delivery.presentation.MainViewModel
 import ru.nikpanfilov.delivery.shared.cities.di.CitiesModule
+import ru.nikpanfilov.delivery.shared.deliveryinfo.di.DeliveryOptionModule
 import ru.nikpanfilov.delivery.shared.user.di.UserModule
 import javax.inject.Singleton
 
@@ -31,6 +32,7 @@ import javax.inject.Singleton
 		SignInModule::class,
 		UserModule::class,
 		ProfileModule::class,
+		DeliveryOptionModule::class,
 	]
 )
 @Singleton
